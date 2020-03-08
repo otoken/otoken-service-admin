7868b317ccc90c450b58d83ec40bfb0a6da00405
liyizhe <liyizhe@yzztech.com>
2020-03-08 13:04:55 +0800

    feat: init project

---
+++ src/main/java/site/azhe/otoken/service/admin/ServiceApplication.java
@@ -0,0 +1,15 @@
+package site.azhe.otoken.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("site.azhe.otoken.service.admin.mapper")
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/site/azhe/otoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package site.azhe.otoken.service.admin.mapper;
+
+import site.azhe.otoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
